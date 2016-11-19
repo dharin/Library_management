44ff465ba9687c6f4becf0a0d129324e644fe983
tc03 <dharin.rajgor@gmail.com>
2016-11-19 16:21:02 +0530

    added login form for admin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.validate.js
 //= require turbolinks
-//= require developer
 //= require twitter/bootstrap
+//= require developer
 //= require_tree .
