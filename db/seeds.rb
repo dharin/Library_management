46438b66929751c5b60ee3287f9c16d6c7cb32f1
tc03 <dharin.rajgor@gmail.com>
2016-11-19 15:31:51 +0530

    created user and add role in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# For Role Default Data
+Role.create([
+    {name: "admin"},
+    {name: "staff"},
+    {name: "student"}])
